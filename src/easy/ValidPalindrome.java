6edd994edbf09b21a0327dce072127b8dd8f2fc0
lipengwei <zhanpwprice@163.com>
2017-07-20 20:59:38 +0800

    fix

---
+++ src/easy/ValidPalindrome.java
@@ -0,0 +1,33 @@
+package easy;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 验证回文串 忽略空格 大小写 和 特殊字符
+ * @author 李鹏伟
+ * leetcode 125
+ */
+
+public class ValidPalindrome {
+
+	public static void main(String[] args) {
+		String s = ",.";
+		System.out.println(isPalindrome(s));
+	}
+	
+	public static boolean isPalindrome(String s) {
+		if(s.isEmpty()){
+			return true;
+		}
+		String str = s.replaceAll("\\W", "").toLowerCase();
+		System.out.println(str);
+		char[] ch = str.toCharArray();
+		for(int i = 0;i < ch.length/2; i++){
+			if(ch[i] != ch[ch.length - 1 -i]){
+				return false;
+			}
+		}
+        return true;
+    }
+}
